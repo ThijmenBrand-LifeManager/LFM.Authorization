dcb50ca294eb3302d20cd8876a23338343398cd7
thijmen brand <thijmen@ik.nu>
2024-09-30 21:35:41 +0200

    add git ignore

--- LFM.Authorization/LFM.Authorization.Api/Endpoints/Users.cs
+++ LFM.Authorization/LFM.Authorization.Api/Endpoints/Users.cs
@@ -1,6 +1,17 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
 namespace LFM.Authorization.Endpoints;
 
-public class Users
+public class Users(IHttpContextAccessor httpContextAccessor) : ControllerBase
 {
-    
+    [HttpGet("me")]
+    [Authorize(AuthenticationSchemes = "Bearer")]
+    public async Task<IActionResult> Me()
+    {
+        var userId = httpContextAccessor.HttpContext.User.Identity.Name;
+
+        return Ok(userId);
+    }
 }
--- LFM.Authorization/LFM.Authorization.Api/Extensions/MigrationExtension.cs
+++ LFM.Authorization/LFM.Authorization.Api/Extensions/MigrationExtension.cs
@@ -1,6 +1,16 @@
+using LFM.Authorization.Repository;
+using Microsoft.EntityFrameworkCore;
+
 namespace LFM.Authorization.Extensions;
 
-public class MigrationExtension
+public static class MigrationExtension
 {
-    
+    public static void ApplyMigrations(this IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+
+        using var context = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
+        
+        context.Database.Migrate();
+    }
 }
