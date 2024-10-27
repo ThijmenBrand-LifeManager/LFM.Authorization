080cb2df90d8521a602c052999ed747233879d41
thijmen brand <thijmen@ik.nu>
2024-10-27 20:30:36 +0100

    :)

---
+++ LFM.Authorization/LFM.Authorization.Endpoint/Program.cs
@@ -0,0 +1,27 @@
+﻿using LFM.Authorization.Core.Messages;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Formatting.Compact;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+var appSettingsFilePath = Environment.GetEnvironmentVariable("APPSETTINGS_FILEPATH");
+if (!string.IsNullOrWhiteSpace(appSettingsFilePath))
+    builder.Configuration.AddJsonFile(appSettingsFilePath);
+    
+builder.Services.RegisterMasstransit(builder.Configuration, true);
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .WriteTo.Console(new RenderedCompactJsonFormatter())
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
+var app = builder.Build();
+
+app.Run();
---
+++ LFM.Authorization/LFM.Authorization.Repository/Migrations/20241027190333_add-default-perm-table-v2.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LFM.Authorization.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class adddefaultpermtablev2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DefaultRolePermissions",
+                schema: "identity",
+                columns: table => new
+                {
+                    Role = table.Column<string>(type: "text", nullable: false),
+                    PermissionName = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DefaultRolePermissions", x => new { x.Role, x.PermissionName });
+                    table.ForeignKey(
+                        name: "FK_DefaultRolePermissions_Permissions_PermissionName",
+                        column: x => x.PermissionName,
+                        principalSchema: "identity",
+                        principalTable: "Permissions",
+                        principalColumn: "Name",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DefaultRolePermissions_PermissionName",
+                schema: "identity",
+                table: "DefaultRolePermissions",
+                column: "PermissionName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DefaultRolePermissions",
+                schema: "identity");
+        }
+    }
+}
