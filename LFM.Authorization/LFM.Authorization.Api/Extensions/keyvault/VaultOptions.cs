d25ab39507e0df3f5d5f78696fc84f07e69b68e8
thijmen brand <thijmen@ik.nu>
2024-11-05 09:55:03 +0100

    add

---
+++ LFM.Authorization/LFM.Authorization.Api/Extensions/keyvault/VaultOptions.cs
@@ -0,0 +1,9 @@
+namespace LFM.Common.KeyVault;
+
+public class VaultOptions
+{
+    public string Address { get; set; }
+    public string Role { get; set; }
+    
+    public string Secret { get; set; }
+}
