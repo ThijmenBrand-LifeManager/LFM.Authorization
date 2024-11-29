48c56d984a35b7e54d1ddf9ebfd560094a48f7da
thijmen brand <thijmen@ik.nu>
2024-11-29 15:03:10 +0100

    hmm

--- LFM.Authorization/LFM.Authorization.Endpoint/Program.cs
+++ LFM.Authorization/LFM.Authorization.Endpoint/Program.cs
@@ -1,13 +1,19 @@
-﻿using LFM.Authorization.Application;
+﻿using Azure.Core;
+using LFM.Authorization.Application;
 using LFM.Authorization.Core.Messages;
 using LFM.Authorization.Repository;
+using LFM.Azure.Common.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 using Serilog.Formatting.Compact;
 
 
 var builder = WebApplication.CreateBuilder(args);
+var userManagedIdentityClientId = Environment.GetEnvironmentVariable("Identity__ClientId");
+var tokenCredential = AzureCredentialFactory.GetCredential(userManagedIdentityClientId);
+builder.Services.AddSingleton<TokenCredential>(tokenCredential);
 
 var appSettingsFilePath = Environment.GetEnvironmentVariable("APPSETTINGS_FILEPATH");
 if (!string.IsNullOrWhiteSpace(appSettingsFilePath))
